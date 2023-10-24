21fa94b2c3e450c08b23b3535da5f8629e6fe637
Roman Doronkin <ramzantryvaper@mail.ru>
2023-10-25 01:14:46 +0300

    вторая итерация ревью, разбил логику на нужные функции

--- scripts/index.js
+++ scripts/index.js
@@ -2,39 +2,33 @@
 const cardContainer = document.querySelector('.places__list');
 const templateElem = document.querySelector('#card-template').content;
 
-initialCards.forEach(function createCard(item) {
-  const cardElem = templateElem.cloneNode(true);
-
-  const deleteCardButton = cardElem.querySelector('.card__delete-button');
-
-  deleteCardButton.addEventListener('click', function () {
-    const cardItem = deleteCardButton.closest('.places__item');
-    cardItem.remove();
-  });
+// @todo: Функция удаления карточки
+function deleteCard(event) {
+  const cardRemove = event.target.closest('.places__item');
+  cardRemove.remove();
+}
+// @todo: Функция создания карточки
+function createCard(item, deleteCard) {
+  const cardElem = templateElem.querySelector('.places__item').cloneNode(true);
 
   cardElem.querySelector('.card__image').src = item.link;
   cardElem.querySelector('.card__image').alt = item.name;
   cardElem.querySelector('.card__title').textContent = item.name;
 
-  cardContainer.append(cardElem);
+  const deleteCardButton = cardElem.querySelector('.card__delete-button');
+  deleteCardButton.addEventListener('click', deleteCard);
+
+  return cardElem;
+}
+
+// @todo: функция вставки карточки на страниц
+
+function addCard(item) {
+  cardContainer.append(item);
+}
+
+// @todo: Вывести карточки на страницу, используем цикл forEach
+
+initialCards.forEach(function (item) {
+  addCard(createCard(item, deleteCard));
 });
-
-const addButton = document.querySelector('.profile__add-button');
-const popupAddNewCard = document.querySelector('.popup_type_new-card');
-const closeAddButton = popupAddNewCard.querySelector('.popup__close');
-
-addButton.addEventListener('click', function openPopup() {
-  popupAddNewCard.classList.add('popup_is-opened');
-});
-
-closeAddButton.addEventListener('click', function closePopup() {
-  popupAddNewCard.classList.remove('popup_is-opened');
-});
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
